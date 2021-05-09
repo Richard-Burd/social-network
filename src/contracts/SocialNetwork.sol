8531181af0b68620a3e43b15daa253222676fe10
Richard-Burd <qalbalassadalasfour@gmail.com>
2021-05-09 11:29:53 -0700

    first three tests passing

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -2,12 +2,31 @@ pragma solidity ^0.5.0;
 
 contract SocialNetwork {
   string public name;
-  string public age;
-  string public sign;
+  uint public postCount = 0;
+
+  // Solidity allows us to do s.thing called
+  // mapping which stores key-value data
+  mapping(uint => Post) public posts;
+
+
+
+  // the struct will define all of the attributes
+  // for the post
+  struct Post {
+    uint id;
+    string content;
+    uint tipAmount;
+    address author;
+  }
 
   constructor() public {
     name = "dapp university social network";
-    age = "22";
-    sign = "Hello";
+  }
+
+  // local variables have an _underscore
+  // by convention, but it is not necessary
+  function createPost(string memory _content) public {
+    postCount ++;
+    posts[postCount] = Post(postCount, _content, 0, msg.sender);
   }
 }
--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -10,19 +10,49 @@ require('chai')
 
 // callback function
 // 'accounts' is an array provided by Ganache
-contract('SocialNetwork', (accounts) => {
+contract('SocialNetwork', ([deployer, author, tipper]) => {
   // simple test to see if it was deployed to the blockchain
   let socialNetwork
+
+before(async () => {
+  socialNetwork = await SocialNetwork.deployed()
+})
+
 
   describe('deployment', async () => {
     it('deploys successfully', async () => {
       // if the address is there, then it deployed
-      socialNetwork = await SocialNetwork.deployed()
+      // socialNetwork = await SocialNetwork.deployed()...abstracted above^
       const address = await socialNetwork.address
       assert.notEqual(address, 0x0)
       assert.notEqual(address, '')
       assert.notEqual(address, null)
       assert.notEqual(address, undefined)
     })
+
+    it('has a name', async () => {
+      // socialNetwork = await SocialNetwork.deployed()...abstracted above^
+      const name = await socialNetwork.name()
+      assert.equal(name, 'dapp university social network')
+    })
+  })
+
+  describe('posts', async () => {
+    let result, postCount
+
+    it('creates posts', async () => {
+      result = await socialNetwork.createPost('This is my first post', { from: author })
+      postCount = await socialNetwork.postCount()
+      // SUCCESS
+      assert.equal(postCount, 1)
+    })
+
+//    it('lists posts', async () => {
+//      // TODO: fill this in
+//    })
+//
+//    it('allows users to tip posts', async () => {
+//      // TODO: fill this in
+//    })
   })
 })
