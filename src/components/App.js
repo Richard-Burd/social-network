ee37eda8a521509d606c251155c55ad3bf5f45d9
Richard-Burd <qalbalassadalasfour@gmail.com>
2021-05-13 16:47:27 -0700

    Ch.4 Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
+import Web3 from 'web3'; // this is the thing that is depreciated:
 import logo from '../logo.png';
 import './App.css';
+import Navbar from './Navbar'
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable() // depreciated notice in browser...https://github.com/MetaMask/detect-provider... use: "await window.eth_requestAccounts"
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected.  You should consider MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0]})
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: ''
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
