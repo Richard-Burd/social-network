ba9875bbf870e3cba81f742c92fe81dda2f04670
Richard-Burd <qalbalassadalasfour@gmail.com>
2021-05-13 12:02:37 -0700

    Chapter 3 Complete

--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -11,7 +11,6 @@ require('chai')
 // callback function
 // 'accounts' is an array provided by Ganache
 contract('SocialNetwork', ([deployer, author, tipper]) => {
-  // simple test to see if it was deployed to the blockchain
   let socialNetwork
 
   before(async () => {
@@ -21,8 +20,6 @@ contract('SocialNetwork', ([deployer, author, tipper]) => {
 
   describe('deployment', async () => {
     it('deploys successfully', async () => {
-      // if the address is there, then it deployed
-      // socialNetwork = await SocialNetwork.deployed()...abstracted above^
       const address = await socialNetwork.address
       assert.notEqual(address, 0x0)
       assert.notEqual(address, '')
@@ -31,7 +28,6 @@ contract('SocialNetwork', ([deployer, author, tipper]) => {
     })
 
     it('has a name', async () => {
-      // socialNetwork = await SocialNetwork.deployed()...abstracted above^
       const name = await socialNetwork.name()
       assert.equal(name, 'Dapp University Social Network')
     })
@@ -40,10 +36,12 @@ contract('SocialNetwork', ([deployer, author, tipper]) => {
   describe('posts', async () => {
     let result, postCount
 
-    it('creates posts', async () => {
+    before(async () => {
       result = await socialNetwork.createPost('This is my first post', { from: author })
       postCount = await socialNetwork.postCount()
+    })
 
+    it('creates posts', async () => {
       // SUCCESS
       assert.equal(postCount, 1)
       const event = result.logs[0].args
@@ -59,12 +57,45 @@ contract('SocialNetwork', ([deployer, author, tipper]) => {
       await socialNetwork.createPost('', { from: author }).should.be.rejected
     })
 
-//    it('lists posts', async () => {
-//      // TODO: fill this in
-//    })
-//
-//    it('allows users to tip posts', async () => {
-//      // TODO: fill this in
-//    })
+     it('lists posts', async () => {
+       const post = await socialNetwork.posts(postCount)
+       assert.equal(post.id.toNumber(), postCount.toNumber(), 'id is correct')
+       assert.equal(post.content, 'This is my first post', 'contrent is correct')
+       assert.equal(post.tipAmount, '0', 'tip amount is correct')
+       assert.equal(post.author, author, 'author is correct')
+     })
+
+     it('allows users to tip posts', async () => {
+       // Track the author balance before purchase
+       let oldAuthorBalance
+       oldAuthorBalance = await web3.eth.getBalance(author)
+       oldAuthorBalance = new web3.utils.BN(oldAuthorBalance)
+
+       result = await socialNetwork.tipPost(postCount, { from: tipper, value: web3.utils.toWei('1', 'Ether') })
+
+       // SUCCESS
+       const event = result.logs[0].args
+       assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct')
+       assert.equal(event.content, 'This is my first post', 'contrent is correct')
+       assert.equal(event.tipAmount, '1000000000000000000', 'tip amount is correct')
+       assert.equal(event.author, author, 'author is correct')
+
+       // Check that author received funds
+       let newAuthorBalance
+       newAuthorBalance = await web3.eth.getBalance(author)
+       newAuthorBalance = new web3.utils.BN(newAuthorBalance)
+
+       let tipAmount
+       tipAmount = web3.utils.toWei('1', 'Ether')
+       tipAmount = new web3.utils.BN(tipAmount)
+
+       const expectedBalance = oldAuthorBalance.add(tipAmount)
+
+       assert.equal(newAuthorBalance.toString(), expectedBalance.toString())
+
+       // FAILURE: Tries to tip a post that does not exist
+       await socialNetwork.tipPost(99, { from: tipper, value: web3.utils.toWei('1', 'Ether')}).should.be.rejected;
+
+     })
   })
 })
